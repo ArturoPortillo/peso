eaafefbf77caa43469d54a78c79fe2bcf82bc782
Arturo <Arturojportillo@gmail.com>
2023-10-24 23:02:40 -0300

    Js

--- peso.js
+++ peso.js
@@ -4,12 +4,13 @@ let esNumero = false;
 do {
   peso = prompt("Ingresa tu peso: ");
   if (peso === null) {
+
     break;
   }
   esNumero = !isNaN(peso) && peso !== '';
 
   if (!esNumero) {
-    alert("Ingresa tu peso!");
+    alert("No ingresaste un valor válido para el peso. Inténtalo de nuevo.");
   } else {
     alert("Tu peso es: " + peso);
   }
